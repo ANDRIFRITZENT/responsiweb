35a504ab3949a250e0be818db813d76df15eb5ac
ANDRIFRITZENT <andryfritzent05@gmail.com>
2019-05-23 09:26:21 +0700

    Updates

---
+++ precache-manifest.4808ce3fb9ea491cc6692353fed78279.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efc5663455e34c58bd1a552eca9e5b4",
+    "url": "/responsiweb/index.html"
+  },
+  {
+    "revision": "a8add6445bd880db68f7",
+    "url": "/responsiweb/static/css/main.7b08123c.chunk.css"
+  },
+  {
+    "revision": "ea05c49ebe27130f1347",
+    "url": "/responsiweb/static/js/2.6eb1fbe1.chunk.js"
+  },
+  {
+    "revision": "a8add6445bd880db68f7",
+    "url": "/responsiweb/static/js/main.6a9e57e6.chunk.js"
+  },
+  {
+    "revision": "adae9c95a9a51b7aacd3",
+    "url": "/responsiweb/static/js/runtime~main.a04c41a8.js"
+  }
+]);
